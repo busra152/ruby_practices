eead3ac09962cafd7c1408a73766f6cf73dfdc68
busra152 <busra.kasim@iwallet.com.tr>
2020-01-10 10:56:10 +0300

    emlak.rb

---
+++ emlak.rb
@@ -0,0 +1,54 @@
+class Emlak
+    attr_accessor  :tip ,:oda_sayisi ,:fiyat ,:il
+
+    def initialize(tip,oda_sayisi,fiyat,il)
+        @tip=tip
+        @oda_sayisi=oda_sayisi
+        @fiyat=fiyat
+        @il=il 
+       
+    end
+    def to_s
+        <<-TAG
+        "tip: #{tip}"
+        "oda sayısı: #{oda_sayisi}"
+        "fiyat: #{fiyat}"
+        "il: #{il}"
+        
+    TAG
+    end
+    def adet
+        puts "adet : #@@count"
+    end
+end
+dizi=[]
+while true
+    puts "komutu girin"
+    x=gets.chomp
+    if x=="ekle"
+        puts "tipi girin:"
+        tip=gets.chomp
+        puts "oda sayısı:"
+        oda_sayisi=gets.chomp
+        puts "fiyatı:"
+        fiyat=gets.chomp
+        puts "il:"
+        il=gets.chomp
+        ekle=Emlak.new(tip,oda_sayisi,fiyat,il)
+        
+        dizi.push(ekle)
+    elsif x=="listele" 
+        puts "-----------------"
+        dizi.each do |items|
+            puts items
+            puts "-----------------"
+        end
+        puts dizi.length
+       
+ 
+    elsif x=="çıkış"
+        exit
+        
+    end
+end
+
